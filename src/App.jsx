3bb189cd96e8b271ec47e25fd1012c3674553176
oscar <oscarmancilla227@gmail.com>
2025-12-11 14:25:31 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">App funcionando</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import App from "./App.jsx";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
